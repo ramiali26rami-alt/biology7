6ce3aad286cc89bd7591c5d1e95e1e6272941816
ramiali26rami-alt <ramiali26rami@gmail.com>
2026-08-09 22:49:23 +0300

    \style: change Android app display name to الأحياء"

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.biotech.biology',
-  appName: 'Smart Bio',
+  appName: 'الأحياء',
   webDir: 'dist'
 };
 
